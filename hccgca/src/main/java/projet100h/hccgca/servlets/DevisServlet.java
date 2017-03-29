1cd113d9efb1db5001ea432d804f2b5ecd32ef9b
thibaulthans <thibault.hans@hei.fr>
2017-03-29 16:15:31 +0200

    bonjour

---
+++ hccgca/src/main/java/projet100h/hccgca/servlets/DevisServlet.java
@@ -0,0 +1,28 @@
+package projet100h.hccgca.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+@WebServlet("/devis")
+public class DevisServlet extends AbstractGenericServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		TemplateEngine templateEngine = this.createTemplateEngine(req);
+		
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+		
+		templateEngine.process("devis", context, resp.getWriter());
+	}
+
+	
+}
