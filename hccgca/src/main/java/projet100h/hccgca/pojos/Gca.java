08dfbcd795ec052c09a7bebff7acd19ef3ba68dd
hugodsgr <hugo.desaegher@hei.yncrea.fr>
2017-04-10 14:56:42 +0200

    Add files via upload

---
+++ hccgca/src/main/java/projet100h/hccgca/pojos/Gca.java
@@ -0,0 +1,28 @@
+package projet100h.hccgca.pojos;
+
+public class Gca {
+
+	private String titreGca;
+	private String texteGca;
+	
+	public Gca(String titreGca, String texteGca) {
+		super();
+		this.titreGca = titreGca;
+		this.texteGca = texteGca;
+	}
+	
+	public String getTitreGca() {
+		return titreGca;
+	}
+	public void setTitreGca(String titreGca) {
+		this.titreGca = titreGca;
+	}
+	public String getTexteGca() {
+		return texteGca;
+	}
+	public void setTexteGca(String texteGca) {
+		this.texteGca = texteGca;
+	}
+	
+	
+}
---
+++ hccgca/src/main/java/projet100h/hccgca/pojos/Hcc.java
@@ -0,0 +1,28 @@
+package projet100h.hccgca.pojos;
+
+public class Hcc {
+
+	private String titreHcc;
+	private String texteHcc;
+	
+	public Hcc(String titreHcc, String texteHcc) {
+		super();
+		this.titreHcc = titreHcc;
+		this.texteHcc = texteHcc;
+	}
+	
+	public String getTitreHcc() {
+		return titreHcc;
+	}
+	public void setTitreHcc(String titreHcc) {
+		this.titreHcc = titreHcc;
+	}
+	public String getTexteHcc() {
+		return texteHcc;
+	}
+	public void setTexteHcc(String texteHcc) {
+		this.texteHcc = texteHcc;
+	}
+	
+	
+}
