4d6cf05985df0f104c0173266ecaab9578b203b8
thibaulthans <thibault.hans@hei.fr>
2017-04-22 14:26:16 +0200

    Update base.js

--- hccgca/src/main/webapp/js/base.js
+++ hccgca/src/main/webapp/js/base.js
@@ -2,10 +2,6 @@
 
 function testScroll(ev){
   var elmt = document.querySelector(".navbar-default");
-  var elmt1 = document.querySelector(".div_milieu_s");
-  var elmt2 = document.querySelector(".div_a_s");
-  var elmt3 = document.querySelector(".div_g_s");
-  var elmt4 = document.querySelector(".style_logo_s");
   var elmt5 = document.querySelector(".navbar-brand");
     if(window.pageYOffset<10){
       elmt.style.paddingTop="10px";
