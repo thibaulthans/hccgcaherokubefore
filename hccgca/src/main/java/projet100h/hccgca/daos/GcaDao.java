def8bda3b7883d9ca66f3755de46d2d9ab43ab15
hugodsgr <hugo.desaegher@hei.yncrea.fr>
2017-04-25 17:14:48 +0200

    Add files via upload

--- hccgca/src/main/java/projet100h/hccgca/daos/GcaDao.java
+++ hccgca/src/main/java/projet100h/hccgca/daos/GcaDao.java
@@ -4,32 +4,32 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import projet100h.hccgca.pojos.Gca;
 
 public class GcaDao {
 
-public Gca addGca(Gca gca) {
+public Gca addGca(String titreGca, String texteGca) {
 	try {
 		Connection connection = DataSourceProvider.getDataSource().getConnection(); 
-		PreparedStatement statement = connection.prepareStatement("INSERT INTO `gca`(`idGca`, `titreGca`, `texteGca`) VALUES(?,?,?)");
-		statement.setString(1, gca.getIdGca());
-		statement.setString(2, gca.getTitreGca());
-		statement.setString(3, gca.getTexteGca());
+		PreparedStatement statement = connection.prepareStatement("INSERT INTO `gca`(`titreGca`, `texteGca`) VALUES(?,?)");
+		statement.setString(1, titreGca);
+		statement.setString(2, texteGca);
 		
-		return gca;
+		return new Gca(titreGca, texteGca);
 	}catch (SQLException e) {
 		e.printStackTrace();
 	}
 	return null;
 }
 
-public void deleteGca(Gca gca){
+public void deleteGca(String idGca){
 	try {
 		Connection connection = DataSourceProvider.getDataSource().getConnection(); 
 		PreparedStatement statement = connection.prepareStatement("DELETE FROM gca WHERE idGca=?");
-		statement.setString(1, gca.getIdGca());
+		statement.setString(1, idGca);
 		statement.executeUpdate();	
 		statement.close();
 	}catch (SQLException e) {
@@ -41,10 +41,10 @@ public void deleteGca(Gca gca){
 public void updateGca(String idGca, String titreGca, String texteGca) {
 	try {
 		Connection connection = DataSourceProvider.getDataSource().getConnection(); 
-		PreparedStatement statement = connection.prepareStatement("UPDATE gca SET idGca=?, titreGca=?, texteGca=?)");
-		statement.setString(1, idGca);
-		statement.setString(2, titreGca);
-		statement.setString(3, texteGca);
+		PreparedStatement statement = connection.prepareStatement("UPDATE gca SET titreGca=?, texteGca=? WHERE idGca=?");
+		statement.setString(1, titreGca);
+		statement.setString(2, texteGca);
+		statement.setString(3, idGca);
 		statement.executeUpdate();
 		statement.close();
 connection.close();
@@ -61,7 +61,7 @@ public Gca getGcaById(String idGca) {
 		statement.setString(1, idGca);
 			ResultSet rs = statement.executeQuery();
 		if (rs.next()) {
-			 gca = new Gca(rs.getString("idGca"), rs.getString("titreGca"), rs.getString("texteGca"));
+			 gca = new Gca(rs.getString("idGca"), rs.getString("titreGca"),rs.getString("texteGca"));
 		}
 		rs.close();
 		statement.close();
@@ -70,6 +70,24 @@ connection.close();
 		e.printStackTrace();
 	}
 	return gca;
+}
+
+public List<Gca> getGca() {
+	ArrayList<Gca> lstgca = new ArrayList<>(); 
+	try (
+		Connection connection = DataSourceProvider.getDataSource().getConnection();
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM gca")){
+			ResultSet rs = statement.executeQuery();
+		while (rs.next()) {
+			 lstgca.add(new Gca(rs.getString("idGca"), rs.getString("titreGca"),rs.getString("texteGca")));
+		}
+		rs.close();
+		statement.close();
+connection.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return lstgca;
 }
 	
 }
