86d15f715fa7a4148315c9d3c0a59dc2a7d6bedd
hugodsgr <hugo.desaegher@hei.yncrea.fr>
2017-04-27 16:55:25 +0200

    Add files via upload

--- hccgca/src/main/java/projet100h/hccgca/servlets/ContactServlet.java
+++ hccgca/src/main/java/projet100h/hccgca/servlets/ContactServlet.java
@@ -1,6 +1,9 @@
 package projet100h.hccgca.servlets;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+
+import projet100h.hccgca.services.ContactService;
 
 @WebServlet("/contact")
 public class ContactServlet extends AbstractGenericServlet{
@@ -21,7 +26,31 @@ public class ContactServlet extends AbstractGenericServlet{
 		
 		WebContext context = new WebContext(req, resp, req.getServletContext());
 		
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+		Date date = new Date();
+		String dateContact =dateFormat.format(date);
+		System.out.println(dateContact); //2016/11/16 12:08:43
+   
+		
+		
+		
 		templateEngine.process("contact", context, resp.getWriter());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+		// Les inputs sont bien récupérés
+		String idContact =req.getParameter("idContact");
+		String nom =req.getParameter("nom");
+        String mail =req.getParameter("inputemail");
+        String objet =req.getParameter("objet");
+        String message =req.getParameter("inputecrire");
+        String dateContact =req.getParameter("dateContact");
+        
+        ContactService.getInstance().saveNewContact(idContact, nom,mail,objet,message, dateContact);
+        
+        resp.sendRedirect("contact");
 	}
 
 	
--- hccgca/src/main/java/projet100h/hccgca/servlets/DevisServlet.java
+++ hccgca/src/main/java/projet100h/hccgca/servlets/DevisServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import projet100h.hccgca.services.DevisService;
+
 @WebServlet("/devis")
 public class DevisServlet extends AbstractGenericServlet{
 
@@ -22,6 +24,28 @@ public class DevisServlet extends AbstractGenericServlet{
 		WebContext context = new WebContext(req, resp, req.getServletContext());
 		
 		templateEngine.process("devis", context, resp.getWriter());
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+		// Les inputs sont bien récupérés
+		String idDevis =req.getParameter("idDevis");
+        String secteurActivite =req.getParameter("inputSA");
+        String chiffreAffaire =req.getParameter("inputCA");
+        String nbSalarie =req.getParameter("inputNS");
+        String missions =req.getParameter("inputMissions");
+        Integer valeurFacture =Integer.parseInt(req.getParameter("inputVF")); //pas sur du tout de cette ligne
+        String nom =req.getParameter("inputNom");
+        String prenom =req.getParameter("inputPrenom");
+        String mail =req.getParameter("inputMail");
+        String informationsSupplementaires =req.getParameter("inputIS");
+        String dateDevis =req.getParameter("dateContact");
+        
+        DevisService.getInstance().saveNewDevis(idDevis, secteurActivite,chiffreAffaire,nbSalarie,missions, valeurFacture, nom, prenom, mail, informationsSupplementaires, dateDevis);
+        
+        resp.sendRedirect("devis");
+
 	}
 
 	
