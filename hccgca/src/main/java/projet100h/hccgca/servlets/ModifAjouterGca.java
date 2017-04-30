335ef370acbdf2b859ea1029fe0fdbf36f502074
hugodsgr <hugo.desaegher@hei.yncrea.fr>
2017-04-30 21:20:30 +0200

    Add files via upload

---
+++ hccgca/src/main/java/projet100h/hccgca/servlets/ModifAjouterGca.java
@@ -0,0 +1,36 @@
+package projet100h.hccgca.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@WebServlet("/ajoutgca")
+public class ModifAjouterGca extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(req.getServletContext());
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.addDialect(new Java8TimeDialect());	
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+
+		templateEngine.process("modification_ajouter_gca", context, resp.getWriter());
+
+}
+	
+}
