6fd1a5be86fc41f9365bcdcb0e62d1234279e86c
thibaulthans <thibault.hans@hei.fr>
2017-05-08 15:32:54 +0200

    Add files via upload

---
+++ hccgca/src/main/webapp/js/globaljs.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+
+ document.getElementById("href_formulaire_contact").onclick = function() {
+
+    location.href='formulaire_contact';
+
+ };
+
+ document.getElementById("href_formulaire_devis").onclick = function() {
+
+    location.href='formulaire_devis';
+
+ };
+
+ document.getElementById("href_formulaire_recrutement").onclick = function() {
+
+    location.href='formulaire_recrutement';
+
+ };
+
+
+}
